77b6c64f2d8701c7a696e2975079cf7151004a44
Georgi <gorgi82@abv.bg>
2022-07-22 10:35:51 +0300

    test

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.stream.Collectors;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String test = new String("asd");
+
         int number = 5;
         List<Integer> numbers = List.of(1,2,3,4,5,6);
         String name = "Dfsfgah";
